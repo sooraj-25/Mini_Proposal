082b327ef65eabc5bf54553ce1a0770c6415823c
Sooraj S.B <151931131+sooraj-25@users.noreply.github.com>
2026-05-28 16:59:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function createLoveTrail(x, y) {
   love.style.top = y + 'px';
   love.style.pointerEvents = 'none';
   love.style.zIndex = '9999';
-  love.style.fontSize = (5 + Math.random() * 15) + 'px';
+  love.style.fontSize = (5 + Math.random() * 18) + 'px';
   love.style.transition = 'transform 1.2s ease-out, opacity 1.2s ease-out';
 
   document.body.appendChild(love);
